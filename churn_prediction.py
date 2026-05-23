19afa1356ed69e287bbf4ff1c7918843772181d8
02656931 <pritimhetre2104@gmail.com>
2026-05-23 13:46:59 +0530

    Implement churn prediction model using Random Forest
    Added churn prediction model

---
+++ churn_prediction.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+data = pd.read_csv('customer_data.csv')
+
+# Features and target
+X = data.drop('Churn', axis=1)
+y = data['Churn']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Predictions
+predictions = model.predict(X_test)
+
+# Accuracy
+print("Model Accuracy:", accuracy_score(y_test, predictions))
